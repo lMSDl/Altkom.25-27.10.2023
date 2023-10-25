b15f2ea5305eaf1d4056e784b954e2f731cbb23d
Paweł Łęczycki <pawel@msd.info.pl>
2023-10-25 10:28:53 +0200

    Minimal API

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,50 +8,36 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 
 app.UseHttpsRedirection();
 
 app.Use(async (httpContext, next) =>
 {
     Console.WriteLine("Before Use1");
-
     await next(httpContext);
-
     Console.WriteLine("After Use1");
 });
-
-
 app.Use(async (httpContext, next) =>
 {
     Console.WriteLine("Before Use2");
-
     await next(httpContext);
-
     Console.WriteLine("After Use2");
 });
 
 
-app.MapGet("/weatherforecast", () =>
+var values = new List<int>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+    1, 5, 8, 23, 456
+};
+//Minimal API
+app.MapGet("/values", () => values);
+app.MapDelete("/values/{value:int}", /*[Authorize]*/ (int value) => values.Remove(value));
+app.MapPost("/values/{value:int}", (int value) => values.Add(value));
+app.MapPut("/values/{oldValue:int}/{newValue:int}", (int oldValue, int newValue) =>  values[values.IndexOf(oldValue)] = newValue);
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+
+
+
