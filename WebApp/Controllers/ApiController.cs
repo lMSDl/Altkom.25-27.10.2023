e28cf615762b42860697ede539a124cb32541b85
Paweł Łęczycki <pawel@msd.info.pl>
2023-10-25 11:56:15 +0200

    Routing

---
+++ WebApp/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")] //adres naszego kontrolera - w nawiasach kwadratowych nazwa klasy bez "Controller"
+    [ApiController] // oznaczamy nasz kontroler jako API
+    public class ApiController : ControllerBase
+    {
+    }
+}
--- WebApp/Controllers/ValuesController.cs
+++ WebApp/Controllers/ValuesController.cs
@@ -3,9 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Controllers
 {
-    [Route("api/[controller]")] //adres naszego kontrolera - w nawiasach kwadratowych nazwa klasy bez "Controller"
-    [ApiController] // oznaczamy nasz kontroler jako API
-    public class ValuesController : ControllerBase
+    //adnotacje dziedziczone z ApiController
+    //[Route("api/[controller]")]
+    //[ApiController] 
+    public class ValuesController : ApiController
     {
         public ValuesController(List<int> values)
         {
@@ -14,21 +15,29 @@ namespace WebApp.Controllers
 
         List<int> _values;
 
-
-        [HttpGet]
+        //GET: localhost:<port>/api/values
+        [HttpGet] //adnotacja określająca typ/czasownik zapytania
         public IEnumerable<int> Get()
         {
             return _values;
         }
 
-        [HttpPost("{value:int:max(50)}")]
+        //Post: localhost:<port>/api/values/<value>
+        [HttpPost("{value:int:max(50)}")] //routing doklejany do adresu kontrolera
+        [HttpPost("alamakota/{value:int:max(50)}")] //metoda dostępna pod dodatkowym adresem /api/[controller]/alamakota/...
         public void Post(int value)
         {
             _values.Add(value);
         }
 
-        [HttpPut("{oldValue:int}/{newValue:int}")]
+        [HttpPut("/alamakota/{oldValue:int}/{newValue:int}")] // ukośnik "/" na początku routingu powoduje, że nie doklejamy do adresu kontrolera, ale tworzymy nowy adres od roota
         public void Put(int oldValue, int newValue)
+        {
+            _values[_values.IndexOf(oldValue)] = newValue;
+        }
+
+        [HttpPut("{oldValue:int}")]
+        public void Put2(int oldValue, int newValue)
         {
             _values[_values.IndexOf(oldValue)] = newValue;
         }
