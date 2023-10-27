2b76c5e0fe15a40a216467f423ebf51385eea706
Paweł Łęczycki <pawel@msd.info.pl>
2023-10-27 15:17:14 +0200

    HealthChecks

---
+++ HealthChecks/Program.cs
@@ -0,0 +1,27 @@
+using HealthChecks.Services;
+using HealthChecks.UI.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks()
+    .AddSqlServer("Data Source=(local)\\SQLExpress;Database=dotnet;Integrated security=true;TrustServerCertificate=True")
+    .AddCheck<DirectoryAccessHealth>(nameof(DirectoryAccessHealth));
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+
+
+var app = builder.Build();
+
+
+
+app.MapHealthChecks("/Health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.MapHealthChecksUI();
+
+app.MapGet("/", () => "Hello World!");
+
+
+app.Run();
